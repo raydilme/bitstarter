b241ad00fadde776991cc0fac2142d017ceb0c96
Ubuntu <ubuntu@ip-172-31-41-78.us-west-2.compute.internal>
2015-10-20 19:42:11 +0000

    index.js

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync(index.html[,encoding]))
-})
+  response.send("This is not easy!")
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
