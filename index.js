255e1f887fc7e33ea4fe4a3540c107dad1c3e681
raydilme <invictus016@hotmail.com>
2015-10-20 19:55:41 +0000

    index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
   response.send("This is not easy!")
-
+})
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
